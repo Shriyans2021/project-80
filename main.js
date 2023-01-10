748431d5606f0f5691346f7bfc2962bc3f077c85
Shriyans2021 <81604852+Shriyans2021@users.noreply.github.com>
2023-01-09 17:41:10 -0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+name_array = [];
+function submit()
+{
+for (var j = 1; j=4; j++)
+{
+var studentname = document.getElementById("name_of_the_student_"+j).value;
+console.log(studentname);
+name_array.push(studentname);
+}
+console.log(studentname);
+}
